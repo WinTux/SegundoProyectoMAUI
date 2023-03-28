d14a275b9aa6b6e33480377a9baca148a4068a60
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-03-27 21:26:34 -0400

    Agregar archivos de proyecto.

---
+++ SegundoProyectoMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SegundoProyectoMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SegundoProyectoMAUI/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ZXing.Net.Maui;
+
+namespace SegundoProyectoMAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	async void OnCodigoBarrasDetectado(object sender, BarcodeDetectionEventArgs e)
+	{
+		//Completar
+	}
+}
+
