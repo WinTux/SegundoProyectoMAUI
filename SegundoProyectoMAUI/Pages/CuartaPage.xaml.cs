16779320e28c0b886e5f442972a97f34a7891f1c
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-04-03 20:54:41 -0400

    Manejo mapas - parte 2

--- SegundoProyectoMAUI/Pages/CuartaPage.xaml.cs
+++ SegundoProyectoMAUI/Pages/CuartaPage.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.Maui.Maps;
 using Microsoft.Maui.Controls.Maps;
+using System.Diagnostics;
 
 namespace SegundoProyectoMAUI.Pages;
 
@@ -19,5 +20,42 @@ public partial class CuartaPage : ContentPage
 		});
 
 		//mimapa.MapType = MapType.Satellite;
+    }
+
+	//Event handler para clic en el mapa
+	async void OnMapaClic(object sender, MapClickedEventArgs e) {
+		Debug.WriteLine($"[MapClicked] {e.Location.Latitude} {e.Location.Longitude}");
+		Polygon recuadro = new Polygon {
+			StrokeWidth = 8,
+			StrokeColor = Color.FromArgb("#00AADD"),
+			FillColor = Color.FromArgb("#8800AADD"),
+			Geopath = { 
+				new Location(-16.500872902488986, -68.13283585711244),
+                new Location(-16.50102849289487, -68.13270040555715),
+                new Location(-16.50114036360522, -68.13283719821695),
+                new Location(-16.500975772192856, -68.13296057983167)
+            }
+        };
+		mimapa.MapElements.Add(recuadro);
+
+		Polyline polyline = new Polyline {
+            StrokeWidth = 18,
+            StrokeColor = Color.FromArgb("#00AADD"),
+            Geopath = {
+                new Location(-16.501049066827807, -68.1322980742047),
+                 new Location(-16.50102977876982, -68.13351311488884),
+                  new Location(-16.50149783510087, -68.13287206693407)
+            }
+        };
+        mimapa.MapElements.Add(polyline);
+
+		Circle circulo = new Circle {
+			Center = new Location(-16.50422255954157, -68.1311709570947),
+			Radius = new Distance(200),
+			StrokeColor = Color.FromArgb("#AAAA11"),
+			StrokeWidth = 15,
+			FillColor = Color.FromArgb("#99AAAA11")
+        };
+		mimapa.MapElements.Add(circulo);
     }
 }
